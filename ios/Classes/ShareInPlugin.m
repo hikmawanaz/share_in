aeb39dfc544fb9f2dc4296c0ca83d30d7e1b5df6
hikmawanaz <hikmawan1232@gmail.com>
2022-04-12 09:26:29 +0700

    feat: initial project

---
+++ ios/Classes/ShareInPlugin.m
@@ -0,0 +1,20 @@
+#import "ShareInPlugin.h"
+
+@implementation ShareInPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"share_in"
+            binaryMessenger:[registrar messenger]];
+  ShareInPlugin* instance = [[ShareInPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
